d4b02ab1ddbb5d286228f5bd7fdab39e0395c296
Pepekid <j.ocanapuigdevall@gmail.com>
2024-07-24 13:12:50 +0200

    added main.js code

--- vite_interactive-rating-component-main/src/main.js
+++ vite_interactive-rating-component-main/src/main.js
@@ -1,3 +1,43 @@
 import './styles/main.scss';
 
-console.log('Hello Vite, desde el proyecto de pepe');
+// Selectors
+const submitBtn = document.querySelector('#submit-btn');
+const ratingButtons = document.querySelectorAll('.rating');
+const ratingSection = document.querySelector('#rating-sect');
+const thankSection = document.querySelector('#thank-sect');
+let selectedRating = null;
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    ratingButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Remove 'selected' class from all buttons
+            ratingButtons.forEach(btn => btn.classList.remove('selected', 'button-selected'));
+            // Add 'selected' class to the clicked button
+            button.classList.add('selected');
+            button.classList.add('button-selected');
+            // Set the selected rating value
+            selectedRating = button.value;
+            console.log('object');
+        });
+    });
+});
+
+// Handle form submit event
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    // Selectors
+    const valueSpan = document.querySelector('.value');
+
+    if (selectedRating) {
+        valueSpan.textContent = selectedRating;
+        ratingSection.classList.add('hidden');
+        thankSection.classList.remove('hidden');
+    } else {
+        console.log('no rating submitted');
+    }
+});
+
