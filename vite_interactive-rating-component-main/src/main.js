7e3ca0cede035356762d3c049d8ccc5271669db5
Josep Ocaña <j.ocanapuigdevall@gmail.com>
2024-08-01 09:25:02 +0200

    solvet hidden section and added mobile design

--- vite_interactive-rating-component-main/src/main.js
+++ vite_interactive-rating-component-main/src/main.js
@@ -7,37 +7,34 @@ const ratingSection = document.querySelector('#rating-sect');
 const thankSection = document.querySelector('#thank-sect');
 let selectedRating = null;
 
-
-
 document.addEventListener('DOMContentLoaded', function () {
-
-    ratingButtons.forEach(button => {
-        button.addEventListener('click', function () {
-            // Remove 'selected' class from all buttons
-            ratingButtons.forEach(btn => btn.classList.remove('selected', 'button-selected'));
-            // Add 'selected' class to the clicked button
-            button.classList.add('selected');
-            button.classList.add('button-selected');
-            // Set the selected rating value
-            selectedRating = button.value;
-            console.log('object');
-        });
-    });
+	ratingButtons.forEach(button => {
+		button.addEventListener('click', function () {
+			// Remove 'selected' class from all buttons
+			ratingButtons.forEach(btn =>
+				btn.classList.remove('selected', 'button-selected')
+			);
+			// Add 'selected' class to the clicked button
+			button.classList.add('selected');
+			button.classList.add('button-selected');
+			// Set the selected rating value
+			selectedRating = button.value;
+		});
+	});
 });
 
 // Handle form submit event
 submitBtn.addEventListener('click', function (e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    // Selectors
-    const valueSpan = document.querySelector('.value');
+	// Selectors
+	const valueSpan = document.querySelector('.value');
 
-    if (selectedRating) {
-        valueSpan.textContent = selectedRating;
-        ratingSection.classList.add('hidden');
-        thankSection.classList.remove('hidden');
-    } else {
-        console.log('no rating submitted');
-    }
+	if (selectedRating) {
+		valueSpan.textContent = selectedRating;
+		ratingSection.classList.add('hidden');
+		thankSection.classList.remove('hidden');
+	} else {
+		console.log('no rating submitted');
+	}
 });
-
